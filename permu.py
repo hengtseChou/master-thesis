83f1b2ffc6b3148ed5050a4088f333707b204a4c
Hank Chou <htchou@protonmail.com>
2024-04-24 11:15:06 +0800

    include res checking in permu.py (no result yet)

--- permu.py
+++ permu.py
@@ -1,69 +1,52 @@
-from itertools import permutations
-from three_lvl_designs.utils import addup, power
-from three_lvl_designs.designs import full_factorial
-from math import factorial
-from tqdm import tqdm
+from itertools import permutations, combinations, product
+from three_lvl_designs.designs import full_factorial_effects
 import numpy as np
 
-effects = [
-    (1, 0, 0),
-    (2, 0, 0),
-    (0, 1, 0),
-    (0, 2, 0),
-    (0, 0, 1),
-    (0, 0, 2),
-    (1, 1, 0),
-    (2, 2, 0),
-    (0, 1, 1),
-    (0, 2, 2),
-    (1, 0, 1),
-    (2, 0, 2),
-    (1, 2, 0),
-    (2, 1, 0),
-    (0, 1, 2),
-    (0, 2, 1),
-    (1, 0, 2),
-    (2, 0, 1),
-    (1, 1, 1),
-    (2, 2, 2),
-    (1, 1, 2),
-    (2, 2, 1),
-    (1, 2, 1),
-    (2, 1, 2),
-    (1, 2, 2),
-    (2, 1, 1),
-]
+effects = full_factorial_effects(k=2, offset=False)
 
-def mod_3(a):
+
+def mod_3(a: np.ndarray):
     return a % 3
-# pbar = tqdm(total=factorial(len(effects)))
-# for per in permutations(effects, len(effects)):
-#     ab = []
-#     ab_squared = []
-#     for pair in zip(effects, per):
-#         ab.append(mod_3(addup(pair[0], pair[1])))
-#         ab_squared.append(mod_3(addup(pair[0], power(pair[1], 2))))
-#     # done third group and fourth group computation
-#     pbar.update(1)
-#     if len(set(ab)) == len(effects) and len(set(ab_squared)) == len(effects):
-#         print(per)
-#         break
+
+
+def offset_effect(a: np.ndarray):
+    return (a * 2) % 3
+
 
 effects_np = [np.array(effect) for effect in effects]
+count = 0
 
-pbar = tqdm(total=factorial(len(effects)))
-for per in permutations(effects_np, len(effects_np)):
-    ab = []
-    ab_squared = []
-    for pair in zip(effects_np, per):
-        ab.append(mod_3(pair[0] + pair[1]))
-        ab_squared.append(mod_3(pair[0] + pair[1] ** 2))
-    # done third group and fourth group computation
-    pbar.update(1)
-    if len(set(map(tuple, ab))) == len(effects_np) and len(
-        set(map(tuple, ab_squared))
-    ) == len(effects_np):
-        print(per)
-        break
+# fix left-hand side of the permutation (group alpha)
+for per_ in permutations(effects_np, len(effects_np)):
+    # try permutation by right hand side (group beta)
+    for per in permutations(effects_np, len(effects_np)):
+        ab = []
+        ab_squared = []
+        for pair in zip(per_, per):
 
+            ab.append(mod_3(pair[0] + pair[1]))
+            ab_squared.append(mod_3(pair[0] + pair[1] * 2))
 
+        ab_no_duplicated = len(set(map(tuple, ab))) == len(effects_np)
+        ab_squared_no_duplicated = len(set(map(tuple, ab_squared))) == len(effects_np)
+
+        if not ab_no_duplicated or not ab_squared_no_duplicated:
+            continue
+
+        # check the alpha group has res 3
+        has_res_three = True
+        for comb in combinations(per, 3):
+            comb_extend = [[e, offset_effect(e)] for e in comb]
+
+            for idxs in product([0, 1], repeat=3):
+
+                add_up_and_mod = mod_3(
+                    comb_extend[0][idxs[0]]
+                    + comb_extend[1][idxs[1]]
+                    + comb_extend[2][idxs[2]]
+                )
+                if (add_up_and_mod == np.array((0, 0))).all():
+                    has_res_three = False
+
+        if has_res_three:
+            print(per)
