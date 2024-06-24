49b7ab0f0f9cbe80f0593f21f50812d9b923ced9
hengtseChou <hengtse.me@gmail.com>
2024-06-24 17:26:20 +0800

    created criteria.R for studying criteria for SOA properties

---
+++ criteria.R
@@ -0,0 +1,16 @@
+hamming_dist <- function(xi, xj) {
+    sum(xi != xj)
+}
+
+moment_abberation <- function(design, pth_moment) {
+    n <- nrow(design)
+    p <- ncol(design)
+    moment_abbr <- 0
+    for (i in 1:(n-1)) {
+        for (j in (i+1):n) {
+          coincidences <- p - hamming_dist(design[i, ], design[j, ])
+          moment_abbr <- moment_abbr + coincidences ^ pth_moment
+        }
+    }
+    return(moment_abbr)
+}
