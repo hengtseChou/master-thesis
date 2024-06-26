8ec61fa84b60554f6872911a76bd1d17d43751d6
hengtseChou <hengtse.me@gmail.com>
2024-06-26 11:14:43 +0800

    add minimum monent abbr functions

--- criteria.R
+++ criteria.R
@@ -14,3 +14,16 @@ moment_abberation <- function(design, pth_moment) {
     }
     return(moment_abbr * (n * (n-1) / 2) ^ -1)
 }
+
+# n runs, m factors, s levels
+minimum_M1 <- function(n, m, s) {
+  (m * (n-s)) / ((n-1) * s)
+}
+
+minimum_M2 <- function(n, m, s) {
+  (n * m * (m+s-1) - (m * s^2)) / ((n-1) * s^2)
+}
+
+minimum_M3 <- function(n, m, s) {
+  (n * m * (m^2 + 3 * m * s + s^2 - 3 * m - 3 * s + 2) - (m * s)^3) / ((n-1) * s^3)
+}
