46b317266a43bc9522f6bb6b0fe2f08a809a403c
hengtseChou <hengtse.me@gmail.com>
2024-06-26 11:14:07 +0800

    add quick go-to for designs

---
+++ source.R
@@ -0,0 +1,8 @@
+setwd("~/nthu-stat/research")
+source("stratification.R")
+source("criteria.R")
+# s levels, k independent factors
+source("designs_with_alpha/s2_k4.R")
+source("designs_with_alpha/s2_k6.R")
+source("designs_with_alpha/s3_k4.R")
+source("designs_with_alpha/s3_k6.R")
