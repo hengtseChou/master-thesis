c2435f39769aaa130bd5e08e8560387ea4bfb833
hengtseChou <hengtse.me@gmail.com>
2024-07-01 16:25:52 +0800

    rename

--- experiment.R
+++ experiment.R
@@ -6,3 +6,9 @@ source("designs_with_alpha/s2_k4.R")
 source("designs_with_alpha/s2_k6.R")
 source("designs_with_alpha/s3_k4.R")
 source("designs_with_alpha/s3_k6.R")
+
+pth_power_moment(D, 1)
+pth_power_moment(D, 2)
+
+minimum_M1(16, 5, 2)
+minimum_M2(16, 5, 2)
