91f6731e47a442cb27a1d144883eacacdbad8e75
hengtseChou <hengtse.me@gmail.com>
2024-07-04 22:47:53 +0800

    fix notations

--- criteria.R
+++ criteria.R
@@ -1,29 +1,30 @@
 hamming_dist <- function(xi, xj) {
-    sum(xi != xj)
+  sum(xi != xj)
 }
 
 pth_power_moment <- function(design, pth_moment) {
-    n <- nrow(design)
-    p <- ncol(design)
-    moment_abbr <- 0
-    for (i in 1:(n-1)) {
-        for (j in (i+1):n) {
-          coincidence <- p - hamming_dist(design[i, ], design[j, ])
-          moment_abbr <- moment_abbr + coincidence ^ pth_moment
-        }
-    }
-    return(moment_abbr * (n*(n-1)/2)^-1)
+  n <- nrow(design)
+  m <- ncol(design)
+  moments_sum <- 0
+  for (i in 1:(n-1)) {
+      for (j in (i+1):n) {
+        coincidence <- m - hamming_dist(design[i, ], design[j, ])
+        moments_sum <- moments_sum + coincidence ^ pth_moment
+      }
+  }
+  return(moments_sum * (n*(n-1)/2)^-1)
 }
 
 # n runs, m factors, s levels
-minimum_M1 <- function(n, m, s) {
+# should check for the correct number of levels in the design
+minimum_K1 <- function(n, m, s) {
   (m*(n-s)) / ((n-1)*s)
 }
 
-minimum_M2 <- function(n, m, s) {
+minimum_K2 <- function(n, m, s) {
   (n*m*(m+s-1) - (m*s)^2) / ((n-1)*s^2)
 }
 
-minimum_M3 <- function(n, m, s) {
+minimum_K3 <- function(n, m, s) {
   (n*m*(m^2 + 3*m*s + s^2 - 3*m - 3*s + 2) - (m*s)^3) / ((n-1)*s^3)
 }
