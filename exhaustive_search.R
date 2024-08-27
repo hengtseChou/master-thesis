78ef0abfc508ec5b8806e8a67628fe5bad796241
hengtseChou <hankthedev@gmail.com>
2024-08-27 17:16:14 +0800

    add verification and formation to length=100

--- exhaustive_search.R
+++ exhaustive_search.R
@@ -1,17 +1,17 @@
-# ---------------------------------------------------------------------------- #
-#                set working directory as current file location                #
-# ---------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------- #
+#                     set working directory as current file location                     #
+# -------------------------------------------------------------------------------------- #
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# ---------------------------------------------------------------------------- #
-#               ffd81: full catalogue of 3 levels, 81 runs design              #
-# ---------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------- #
+#                    ffd81: full catalogue of 3 levels, 81 runs design                   #
+# -------------------------------------------------------------------------------------- #
 ffd81 <- read.csv("ffd81.csv")
 ffd81_columns <- ffd81$columns
 
-# ---------------------------------------------------------------------------- #
-#                       S81: saturated design of 81 runs                       #
-# ---------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------- #
+#                            S81: saturated design of 81 runs                            #
+# -------------------------------------------------------------------------------------- #
 # the format follows Table 6 from "A catalogue of three-level regular fractional factorial designs"
 full <- function(l, n){ # return an l^n full factorial design
   if (n == 1) return(cbind(1:l-1))
@@ -33,9 +33,9 @@ S81 <- S81[4:1, ]
 rownames(S81) <- c(1, 2, 3, 4)
 # write.csv(as.data.frame(S81), "s81_saturated.csv", row.names = FALSE)
 
-# ---------------------------------------------------------------------------- #
-#          create a dataframe which shows columns that can form a line         #
-# ---------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------- #
+#               create a dataframe which shows columns that can form a line              #
+# -------------------------------------------------------------------------------------- #
 is.one <- function(x) { # check whether the first non-zero element of x is one
   for (i in 1:length(x)) {
     if (x[i] == 0) next
@@ -54,8 +54,8 @@ for (i in 1:39){
     tmp[2, ] <- S81[, j]
     tmp[3, ] <- (S81[, i] + S81[, j]) %% 3
     tmp[4, ] <- (S81[, i] + 2 * S81[, j]) %% 3
-    if(!is.one(tmp[3, ]))tmp[3, ] <- (tmp[3, ] * 2) %% 3
-    if(!is.one(tmp[4, ]))tmp[4, ] <- (tmp[4, ] * 2) %% 3
+    if (!is.one(tmp[3, ])) tmp[3, ] <- (tmp[3, ] * 2) %% 3
+    if (!is.one(tmp[4, ])) tmp[4, ] <- (tmp[4, ] * 2) %% 3
     # form_line_duplicated[k, ] <- tmp %*% 3 ^ (0:3)
     form_line_duplicated[k, ] <- sapply(tmp %*% 3 ^ (0:3), function(x) mapping[[as.character(x)]])
     k <- k + 1
@@ -71,9 +71,9 @@ for (i in 2:nrow(form_line_duplicated)) {
 }
 # write.csv(as.data.frame(form_line), "s81_line.csv", row.names = FALSE)
 
-# ---------------------------------------------------------------------------- #
-#                use ffd81 and form_line to do exhausive search                #
-# ---------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------- #
+#                     use ffd81 and form_line to do exhausive search                     #
+# -------------------------------------------------------------------------------------- #
 # find design A that satisfies thm3 from "STRONG ORTHOGONAL ARRAYS OF STRENGTH TWO PLUS"
 saturated <- 1:40
 good_A <- c()
@@ -94,14 +94,36 @@ for (i in 1:length(ffd81_columns)) {
     good_A <- c(good_A, paste(columns, collapse = " "))
   }
   # if (i %% 1000 == 1) {
-  #   print(paste("completed:", i))
+  #  print(paste("completed:", i))
   # }
 }
+
+# -------------------------------------------------------------------------------------- #
+#                                      saving result                                     #
+# -------------------------------------------------------------------------------------- #
 good_A_idx <- c()
 good_A_num_of_columns <- c()
 for (a in good_A) {
   good_A_idx <- c(good_A_idx, which(ffd81_columns == a))
   good_A_num_of_columns <- ffd81$num_of_columns[good_A_idx]
 }
-data.frame(idx=good_A_idx, num_of_columns=good_A_num_of_columns, columns=good_A) |>
-  write.csv("s81_usable_A.csv", row.names = FALSE)
+result <- data.frame(idx=good_A_idx, num_of_columns=good_A_num_of_columns, columns=good_A)
+write.csv(result, "s81_usable_A.csv", row.names = FALSE)
+
+# -------------------------------------------------------------------------------------- #
+#                                      verification                                      #
+# -------------------------------------------------------------------------------------- #
+idx_to_check_with <- 10
+columns <- as.numeric(unlist(strsplit(good_A[idx_to_check_with], " ")))
+complementary <- setdiff(saturated, columns)
+columns_form_line <- rep(F, length(columns))
+for (j in 1:length(columns)) {
+  form_line_lookup_idx <- which(form_line[, 1] == columns[j])
+  for (k in form_line_lookup_idx) {
+    if (setequal(intersect(complementary, form_line[k, -1]), form_line[k, -1])) {
+      columns_form_line[j] <- T
+      print(paste0("a_j: ", columns[j], ", comp: ", paste(form_line[k, -1], collapse = " ")))
+      break
+    }
+  }
+}
