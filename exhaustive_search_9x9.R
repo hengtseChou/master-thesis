c5206d77332b9862922f1e75d70438b09efddadd
hengtseChou <hankthedev@gmail.com>
2024-09-17 22:21:19 +0800

    9x9 for m=11

--- exhaustive_search_9x9.R
+++ exhaustive_search_9x9.R
@@ -70,23 +70,123 @@ for (i in 2:nrow(form_line_duplicated)) {
   if (!IN) form_line <- rbind(form_line, form_line_duplicated[i, ])
 }
 
-corresponding_bi <- list()
-for (i in 1:40) {
-  bi <- c()
-  for (j in 1:nrow(form_line)) {
-    if (i %in% form_line[j, ]) {
-      bi <- c(bi, form_line[j, -which(form_line[j, ] == i)])
+# ---------------------------------------------------------------------------- #
+#                      load in good A and find possible B                      #
+# ---------------------------------------------------------------------------- #
+
+good_A <- read.csv("s81_good_A_filtered.csv")
+m11_columns <- good_A[1, ]$columns
+m11_columns <- as.numeric(unlist(strsplit(m11_columns, " ")))
+comp <- (1:40)[-m11_columns]
+
+corresponding_b <- list()
+
+for (a in m11_columns) {
+  b <- c()
+  for (i in 1:nrow(form_line)) {
+    line <- form_line[i, ]
+    if (a %in% line) {
+      if (all(line[-which(line == a)] %in% comp)) {
+        b <- c(b, line[-a])
+      }
     }
   }
-  bi <- unique(bi)
-  corresponding_bi[[i]] <- bi
+  corresponding_b[[as.character(a)]] <- unique(b)
 }
 
-factor_add <- function(a, b) {
-  add_up <- S81[, a] + S81[, b]
-  if (!first_is_one(add_up)) add_up <- (add_up * 2) %% 3
-  return(mapping[as.character(add_up %*% 3 ^ (0:3))])
+# ---------------------------------------------------------------------------- #
+#                  create a loop for iterating all possible B                  #
+# ---------------------------------------------------------------------------- #
+
+powers_of_3 <- 3^(0:3)
+mapping_names <- names(mapping)
+
+adding_factors <- function(a, b) {
+  col_a <- S81[, a]
+  col_b <- S81[, b]
+  add_up <- col_a + col_b
+  if (!first_is_one(add_up)) {
+    add_up <- (add_up * 2) %% 3
+  }
+  result_value <- as.numeric(add_up %*% powers_of_3)
+  return(unname(mapping[which(mapping_names == result_value)]))
 }
 
-ai <- read.csv("s81_good_A.csv")
+is_condition_satisifed <- function(ai, aj, bi, bj) {
+  bi_square <- adding_factors(bi, bi)
+  bj_square <- adding_factors(bj, bj)
+  
+  ai_bi <- adding_factors(ai, bi)
+  ai_bi_square <- adding_factors(ai, bi_square)
+  
+  aj_bj <- adding_factors(aj, bj)
+  aj_bj_square <- adding_factors(aj, bj_square)
+  
+  if (anyDuplicated(c(bi, bj, ai_bi, ai_bi_square, aj_bj, aj_bj_square))) {
+    return(FALSE)
+  }
+  return(TRUE)
+}
 
+m <- length(m11_columns)
+max_positions <- lapply(corresponding_b, length) |> unlist()
+curr_position <- rep(1, m)
+names(curr_position) <- names(max_positions)
+
+best_b <- NULL
+best_b_good_pairs <- 0
+current_b <- rep(0, m)
+
+width <- getOption("width") + 3
+padding1 <- strrep(" ", width - 48)
+padding2 <- strrep(" ", width - 44)
+
+while (!all(max_positions == curr_position)) {
+  for (i in 1:m) {
+    current_b[i] <- corresponding_b[[names(curr_position[i])]][curr_position[i]]
+  }
+  if (!anyDuplicated(current_b)) { # no duplication in this B
+    pairs <- combn(m, 2)
+    good_pairs <- 0
+    for (j in 1:ncol(pairs)) {
+      pair <- pairs[, j]
+      if (is_condition_satisifed(
+        m11_columns[pair[1]], m11_columns[pair[2]], current_b[pair[1]], current_b[pair[2]]
+      )) good_pairs <- good_pairs + 1
+    }
+    if (good_pairs > best_b_good_pairs) {
+      best_b_good_pairs <- good_pairs
+      best_b <- current_b
+    }
+    # move to the next design
+    cursor <- 1
+    while (curr_position[cursor] + 1 > max_positions[cursor]) {
+      curr_position[cursor] <- 1
+      cursor <- cursor + 1
+    }
+    curr_position[cursor] <- curr_position[cursor] + 1
+  }
+  # locate duplication and move that position
+  while(anyDuplicated(current_b)) {
+    idx <- head(which(duplicated(current_b, fromLast = T)), 1)
+    if (curr_position[idx] + 1 <= max_positions[idx]) {
+      curr_position[idx] <- curr_position[idx] + 1
+    } else {
+      curr_position[idx] <- 1
+      cursor <- idx + 1
+      while (curr_position[cursor] + 1 > max_positions[cursor]) {
+        curr_position[cursor] <- 1
+        cursor <- cursor + 1
+      }
+      curr_position[cursor] <- curr_position[cursor] + 1
+    }
+    for (i in 1:m) {
+      current_b[i] <- corresponding_b[[names(curr_position[i])]][curr_position[i]]
+    }
+  }
+  # stream current result
+  curr_position_str <- paste0("curr_position: ", paste(sprintf("%-3s", unname(curr_position)), collapse = ""))
+  current_b_str <- paste0("current_b:     ", paste(sprintf("%-3s", unname(current_b)), collapse = ""))
+  best_b_str <- paste0("best_b:        ", paste(sprintf("%-3s", unname(best_b)), collapse = ""))
+  cat("\r", curr_position_str, padding1, current_b_str, padding2, best_b_str, sep = "")
+}
